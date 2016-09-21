9b3d75eb8704e04f13a2e355d24d22d45ffd3b59
Evan Su <evan.universe.suo@gmail.com>
2016-09-21 15:22:12 +1000

    init

---
+++ Example/Pods/Target Support Files/ESPopupDialog/ESPopupDialog-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ESPopupDialogVersionNumber;
+FOUNDATION_EXPORT const unsigned char ESPopupDialogVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-ESPopupDialog_Example/Pods-ESPopupDialog_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ESPopupDialog_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ESPopupDialog_ExampleVersionString[];
+
